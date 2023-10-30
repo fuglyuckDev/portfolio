d95a7788fd50ea74206537fa46e2780e38a3dda7
fuglyuckDev <harrisoncwalton@gmail.com>
2023-10-30 18:01:12 +0000

    Added email usage

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import styles from "./Contact.module.scss";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [toggle, setToggle] = useState(false);
@@ -11,20 +12,36 @@ const Contact = () => {
       : document.getElementById("contact").close();
   }, [toggle]);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.send(
+      "query_email",
+      "quote_email",
+      {
+        email: e.target.email.value,
+        message: e.target.message.value,
+        company: e.target.company.value,
+        name: e.target.name.value,
+      },
+      "rPVlmRtn45wWuAQVP"
+    );
+    console.log("sent");
+  };
+
   return (
     <div>
       <dialog className={styles.dialog} id={"contact"}>
-        <form className={styles.contact__Form}>
+        <form className={styles.contact__Form} onSubmit={(e) => sendEmail(e)}>
           <div className={styles.contact__Form__Container}>
             <label>Email:</label>
-            <input type="email" id="email"></input>
+            <input type="email" id="email" name="email"></input>
             <label>Name:</label>
-            <input type="text" name="fname"></input>
+            <input type="text" name="fname" id="name"></input>
             <label>Company:</label>
-            <input type="text" name="=lname"></input>
+            <input type="text" name="=lname" id="company"></input>
 
             <label>Description:</label>
-            <textarea></textarea>
+            <textarea type="text" id="message" name="message"></textarea>
             <div>
               <button className={styles.send__button} type="submit">
                 Send
