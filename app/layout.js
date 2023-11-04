f6225f3fa327e0cda95906c8e33cf53c284444e5
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-04 23:55:27 +0000

    testing metadata in layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,10 @@ import Navigation from "@/components/Navigation/Navigation";
 
 export const metadata = {
   title: "Harrison Walton - Front End Portfolio",
-  description: "",
+  description: "UX/UI Designer • Front-End Developer",
+  "og:image":
+    "https://harrison-walton-portfolio-fuglyuckdev.vercel.app/images/metadata/portfolio_og.webp",
+  "og:description": "Function, form and everything in between.",
 };
 
 export default function RootLayout({ children }) {
