34ce89e5b3780f6c67fcdf5f897ffbfbf8a5b15d
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-04 23:44:32 +0000

    Added image OG metadata

--- app/page.js
+++ app/page.js
@@ -2,10 +2,25 @@ import About from "@/components/About";
 import Introduction from "@/components/Introduction/Introduction";
 import Projects from "@/components/Projects";
 import ViewCheck from "@/components/ViewCheck";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta
+          property="og:title"
+          content="UX/UI Designer • Front-End Developer"
+        />
+        <meta
+          property="og:description"
+          content="Harrison Walton - Web development portfolio"
+        />
+        <meta
+          property="og:image"
+          content="/images/metadata/portfolio_og.webp"
+        />
+      </Head>
       <Introduction />
       <About />
       <Projects />
