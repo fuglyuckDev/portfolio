0e372ea1a7a9822647e4e67cf2ca6eb7ad68630e
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-05 00:03:07 +0000

    Removed metadata from homepage

--- app/page.js
+++ app/page.js
@@ -2,25 +2,10 @@ import About from "@/components/About";
 import Introduction from "@/components/Introduction/Introduction";
 import Projects from "@/components/Projects";
 import ViewCheck from "@/components/ViewCheck";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
-      <Head>
-        <meta
-          property="og:title"
-          content="UX/UI Designer • Front-End Developer"
-        />
-        <meta
-          property="og:description"
-          content="Harrison Walton - Web development portfolio"
-        />
-        <meta
-          property="og:image"
-          content="https://harrison-walton-portfolio-fuglyuckdev.vercel.app/images/metadata/portfolio_og.webp"
-        />
-      </Head>
       <Introduction />
       <About />
       <Projects />
