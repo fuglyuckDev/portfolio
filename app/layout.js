14e98d1d3e4eb6db52eb74e719c4343ff4c6f139
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-05 00:14:54 +0000

    added image hope it works lol

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import Navigation from "@/components/Navigation/Navigation";
 export const metadata = {
   title: "Harrison Walton - Front End Portfolio",
   description: "UX/UI Designer • Front-End Developer",
+  image:
+    "https://harrison-walton-portfolio-fuglyuckdev.vercel.app/images/metadata/portfolio_og.webp",
 };
 
 export default function RootLayout({ children }) {
